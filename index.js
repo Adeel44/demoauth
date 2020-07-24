1d85315a86c724d89f51ebe84e0b8483160840e3
Adeel44 <adeel.ngxoft@gmail.com>
2020-07-24 08:59:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+
+const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
+
+
+dotenv.config()
+
+
+mongoose.connect(process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (error , clint)=>{
+      if(error){
+     return console.log("Unable  to conect to db")
+      }
+      console.log("BD conect")
+
+})
+
+
+
+
+app.use(express.json())
+
+// //Route middlewae
+ app.use('/api/user' , authRoute)
+ app.use('/api/posts', postRoute)
+
+
+//app.get('/', (req, res) => res.send('Hello World! node'))
+
+app.listen(3000, () => console.log('Server is running on 3000'))
+
